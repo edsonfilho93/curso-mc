168cb651c536f6fb987ecd9dc5aee3f24640a7d6
edson <edsonfilho734@gmail.com>
2019-10-09 00:12:24 -0300

    Implementacao de produtos e suas associacoes

--- src/main/java/br/com/edson/cursomc/domain/Categoria.java
+++ src/main/java/br/com/edson/cursomc/domain/Categoria.java
@@ -1,21 +1,27 @@
 package br.com.edson.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias") // define o outro lado relacionamento muitos para muitos
+	private List<Produto> produtos = new ArrayList<Produto>();
 
 	public Categoria() {
 	}
@@ -42,6 +48,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	} 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -66,5 +80,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
 
 }
