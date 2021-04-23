f4208464d01e830fadd2e8cf469c55e98713b6be
Edson <edsonfilho734@gmail.com>
2021-04-23 01:45:54 -0300

    Criacao de endpoint para pedidos e correcao de referencia cilica

--- src/main/java/br/com/edson/cursomc/domain/Categoria.java
+++ src/main/java/br/com/edson/cursomc/domain/Categoria.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 
 @Entity
+@Data
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -34,55 +35,4 @@ public class Categoria implements Serializable {
 		this.id = id;
 		this.nome = nome;
 	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	} 
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-
 }
